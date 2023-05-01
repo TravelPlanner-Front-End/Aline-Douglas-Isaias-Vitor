874f69cf8bf1727f8be2438c8827956bb97cfc02
Vitor Hugo Reis Moreira <107319103+theonlyvitor@users.noreply.github.com>
2023-05-01 15:50:37 -0400

    feat: adiciona a arquitetura da pagina registro

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,8 +1,14 @@
+import { RegisterUserForm } from "../../components/Form/RegisterUserForm";
+import imageRegister from "../../assets/vector-Register.svg";
+
 export const RegisterPage = () => {
 
     return (
-        <>
-            
-        </>
+        <section>
+            <RegisterUserForm />
+            <div>
+                <img src={imageRegister} alt="" />
+            </div>
+        </section>
     )
 }
