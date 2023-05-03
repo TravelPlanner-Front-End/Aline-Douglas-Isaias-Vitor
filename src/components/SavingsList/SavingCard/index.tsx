8c6b103818a48bfc29c6932b8feaccc436c7bb14
alinecarvalhopro <alinecarvalho.pro@gmail.com>
2023-05-03 14:49:17 -0300

    fix: correcao de bug referente ao id cadastrado

--- src/components/SavingsList/SavingCard/index.tsx
+++ src/components/SavingsList/SavingCard/index.tsx
@@ -1,6 +1,5 @@
 import { ISaving } from "../../../providers/TravelContext";
 import { StyledLi } from "./style";
-import { v4 as uuidv4 } from "uuid";
 
 interface ISavingCardsProps {
   saving: ISaving;
@@ -8,7 +7,7 @@ interface ISavingCardsProps {
 
 export const SavingsCard = ({ saving }: ISavingCardsProps) => {
   return (
-    <StyledLi key={uuidv4()}>
+    <StyledLi>
       <h3>
         Em {saving.month} economizei: <span>{`R$ ${saving.value}`}</span>
       </h3>
--- src/components/SavingsList/index.tsx
+++ src/components/SavingsList/index.tsx
@@ -2,6 +2,7 @@ import { useTravelContext } from "../../providers/TravelContext";
 import { useEffect } from "react";
 import { StyledDiv } from "./style";
 import { SavingsCard } from "./SavingCard";
+import { v4 as uuidv4 } from "uuid";
 
 export const SavingsList = () => {
   const { loadSavings, travel, savings } = useTravelContext();
@@ -25,7 +26,7 @@ export const SavingsList = () => {
     <StyledDiv>
       <ul>
         {savings
-          ? savings.map((saving) => <SavingsCard saving={saving} />)
+          ? savings.map((saving) => <SavingsCard key={uuidv4()} saving={saving} />)
           : null}
       </ul>
       <div>
